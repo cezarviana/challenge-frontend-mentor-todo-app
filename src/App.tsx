edf72df38afa6fa038078056406c8a2133dcd65b
Cezar Viana <cezar.viana@gmail.com>
2026-03-19 16:48:01 -0300

    feat: add header, form and list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
+import TodoForm from './components/TodoForm';
 import TodoHeader from './components/TodoHeader';
-import IconSun from '/images/icon-sun.svg';
+import TodoList from './components/TodoList';
 
 function App() {
 
   return (
     <main className="bg-dark-navy-950 h-lvh">
       <div className="bg-[url('./images/bg-desktop-dark.jpg')] h-80 bg-cover bg-center">
+        <div className=' max-w-[43.75rem] m-auto p-8'>
           <TodoHeader></TodoHeader>
+
+          <TodoForm></TodoForm>
+
+          <TodoList></TodoList>
+
+        </div>
       </div>
     </main>
   )
